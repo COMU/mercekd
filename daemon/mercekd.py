faec3f8f68d1724140efe096cd40521bea5500c0
halilbay <halilbay91@gmail.com>
2012-06-21 10:11:04 +0300

    "logging" bölümü eklendi.

--- daemon/mercekd.py
+++ daemon/mercekd.py
@@ -1,75 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+import tailer
 import os
 import sys
 import logging
-
-#Options Bolumu
-
-yardim= """
-
-"Bu kisim yardim bolumu"""
-
-paths="/home/halil/mercekd"
-
-verbose="verbose kismi"
-
-if len(sys.argv)<2:
-    print "..."
-
-elif sys.argv[1]=="-h":
-    print yardim
-
-elif sys.argv[1]=="-p":
-    print paths
-
-elif sys.argv[1]=="-v":
-    print "Options:\n\n-h\tHelp\n-p\tPath\n-v\tVerbose\n"
-
-else:
-    print "Boyle bir secenek yok"
-
-#Dosya acma bolumu
-try:
-    f=open("/home/halil/mercekd/daemon/leases.txt",'r')
+from optparse import OptionParser
+import datetime
 
 
-except:
-    print  "This file does not exist\n"
+#options
+program='mercekd'
+parser = OptionParser(usage='usage: %prog [options] filename')
+parser.add_option("-p", "--path",type="string",action="store",dest="path", metavar='filename')
+parser.add_option("-v","--verbose",dest="verbose")
+(option,args)=parser.parse_args()
 
 
-#tail bolumu
-def tail( f, window=20 ):
-    BUFSIZ = 1024
-    f.seek(0, 2)
-    bytes = f.tell()
-    size = window
-    block = -1
-    data = []
-    while size > 0 and bytes > 0:
-        if (bytes - BUFSIZ > 0):
-            # Seek back one whole BUFSIZ
-            f.seek(block*BUFSIZ, 2)
-            # read BUFFER
-            data.append(f.read(BUFSIZ))
-        else:
-            # file too small, start from begining
-            f.seek(0,0)
-            # only read what was not read
-            data.append(f.read(bytes))
-        linesFound = data[-1].count('\n')
-        size -= linesFound
-        bytes -= BUFSIZ
-        block -= 1
-    return '\n'.join(' '.join(data).splitlines()[-window:])
+#logging
+log=logging.getLogger("mercekd")
+log.setLevel(logging.DEBUG)
+logger_console=logging.StreamHandler()
+logger_console.setLevel(logging.DEBUG)
+formatter=logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_console.setFormatter(formatter)
+log.addHandler(logger_console)
 
-while True:
+#tail
+if __name__ == "__main__":
+    log.debug(datetime.datetime.now().isoformat(sep=" "))
     try:
-        tail_try=tail(f,window=7)
-        find_1=tail_try.find("lease")
-        find_2=tail_try.find("}",find_1)
-        find_final=tail_try[find_1:find_2]
-        print find_final
+        result=dict()
+        flag = False
+        for line in tailer.follow(open('/home/halil/mercekd/daemon/leases.txt')):
+            if line.startswith('lease'):
+                flag = True
 
+                lease=line.split()
+
+                ip=lease[1]
+                result['ip']=ip
+
+                continue
+
+            if line.strip().startswith("}"):
+                log.setLevel(logging.INFO)
+                log.info("Record")
+                flag = False
+                print "Record:", result
+
+
+            if flag:
+                if line.strip().startswith('starts'):
+                    li = line.strip().split()
+                    start_time=" ".join(li[2:])
+                    result['start_time']=start_time
+
+                    continue
+
+                if line.strip().startswith('ends'):
+                    li = line.strip().split()
+                    end_time=" ".join(li[2:])
+                    result['end_time']=end_time
+                    continue
+
+                if line.strip().startswith('hardware'):
+                    li = line.strip().split()
+                    ethernet=li[2]
+                    result['ethernet']=ethernet
+                    continue
+
+                if line.strip().startswith('uid'):
+                    li = line.strip().split()
+                    uid=li[1]
+                    result['uid']=uid
+                    continue
+
+                if line.strip().startswith('client-hostname'):
+                    li = line.strip().split()
+                    uname=li[1]
+                    result['client-hostname']=uname
+                    continue
+
+        #line.split('\n')
     except KeyboardInterrupt:
-        print "\nThere is an exception"
+        print "There is an exception"
         sys.exit(1)
