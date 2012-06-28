f06d3f3ccbd9b784f4b9ce974ab96ad96b3de788
halilbay <halilbay91@gmail.com>
2012-06-28 17:33:49 +0300

    Sql sorgusu ve log mesajı düzeltildi

--- daemon/sql/sql.py
+++ daemon/sql/sql.py
@@ -6,13 +6,7 @@ import MySQLdb
 from settings import ConfigClass
 import os
 #logging
-log_db=logging.getLogger("mercekd_db")
-log_db.setLevel(logging.ERROR)
-logger_db_console=logging.StreamHandler()
-logger_db_console.setLevel(logging.ERROR)
-formatter=logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_db_console.setFormatter(formatter)
-log_db.addHandler(logger_db_console)
+
 class SqlManager:
     def __init__(self):
         connectionState=0
@@ -23,6 +17,13 @@ class SqlManager:
         config_class.config_file=open(path+"/"+"setting.config",'r')
         config_class.config.readfp(config_class.config_file)
 
+        self.log_db=logging.getLogger("mercekd_db")
+        self.log_db.setLevel(logging.ERROR)
+        self.logger_db_console=logging.StreamHandler()
+        formatter=logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        self.logger_db_console.setFormatter(formatter)
+        self.log_db.addHandler(self.logger_db_console)
+
         try:
             self.server=config_class.config.get("Sql Settings","server")
             self.user_name=config_class.config.get("Sql Settings","user_name")
@@ -30,13 +31,14 @@ class SqlManager:
             self.database_name=config_class.config.get("Sql Settings","database_name")
 
         except ConfigParser.NoOptionError:
-            log_db.error("No option exist this section")
+            self.log_db.error("No option exist this section")
 
     def connect(self):
         try:
             self.conn=MySQLdb.connect(host=self.server,user=self.user_name,passwd=self.passwd,db=self.database_name)
-            #log_db.setLevel(logging.INFO)
-            log_db.info("The connection is successful")
+            self.log_db.setLevel(logging.INFO)
+            self.log_db.info("The connection is successful")
+            self.logger_db_console.flush()
             self.connectionState=1
         except MySQLdb.DatabaseError:
             self.connectionState=0
@@ -45,9 +47,8 @@ class SqlManager:
 
 
     def insert(self, result):
-
         try:
-            sql_query=("""INSERT INTO lease_db(ip,mac,start,end) VALUES (%s,%s,%s,%s)""" ,(result['ip'],result['ethernet'],result['start_time'],result['end_time']))
+            sql_query="INSERT INTO lease_db(`ip`,`mac`,`start`,`end`) VALUES (\"%s\",\"%s\",\"%s\",\"%s\")" % ((result['ip'],result['ethernet'],result['start_time'],result['end_time']))
             result['uid']=None
             result['client-hostname']=None
             cursor=self.conn.cursor()
