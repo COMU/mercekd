20af01802dfdbe2eeaad3daf6c00f877bfa02403
halilbay <halilbay91@gmail.com>
2012-06-28 18:06:15 +0300

    setting.config /mercekd/daemon klasöründe olduğu varsayıldı ve ayarlar  için bu dosyada değişiklik yapmak gerekiyor

--- settings.py
+++ settings.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import ConfigParser
-import os
 
 class ConfigClass:
     def __init__(self):
         self.config=ConfigParser.RawConfigParser()
-        self.config.add_section("Sql Settings")
-        self.config.set("Sql Settings","server","localhost")
-        self.config.set("Sql Settings","user_name","mercekd_user")
-        self.config.set("Sql Settings","passwd","")
-        self.config.set("Sql Settings","database_name","mercekd_db")
+        self.config_file = None
+       # self.config.add_section("Sql Settings")
+        #self.config.set("Sql Settings","server","localhost")
+       # self.config.set("Sql Settings","user_name","root")
+       # self.config.set("Sql Settings","passwd","121212")
+        #self.config.set("Sql Settings","database_name","mercekd_db")
 
-    def read_config(self, path):
-        settings_path = os.path.abspath(path)
-        config_file=open(settings_path,'w')
-        self.config.write(config_file)
-        config_file.close()
+    #def read_config(self, path):
+       # settings_path = os.path.join(path,"setting.config")
+        #config_file=open(settings_path,'w')
+        #self.config.write(config_file)
+        #config_file.close()
 
 if __name__=="__main__":
     config_class=ConfigClass()
