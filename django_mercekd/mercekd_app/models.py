76350b23dea3c2b2681ec90c451e96013f01daba
halilbay <halilbay91@gmail.com>
2012-07-08 01:19:47 +0300

    model.py oluşturuldu fixes #15

---
+++ django_mercekd/mercekd_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class lease_db:
+    id=models.AutoField(primary_key=True)
+    ip=models.IPAddressField(blank=False)
+    mac=models.CharField(max_length=20,blank=False)
+    start=models.DateTimeField(blank=False)
+    end=models.DateTimeField(blank=False)
+    uid=models.CharField(max_length=100,blank=True)
+    client=models.CharField(max_length=100,blank=True)
