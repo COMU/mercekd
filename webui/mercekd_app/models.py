2f84465cd7ab0d7c4437bafdf2696be12507eb28
halilbay <halilbay91@gmail.com>
2012-07-09 10:52:11 +0300

    class yapısı düzeltdildi fixes #17

---
+++ webui/mercekd_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class lease_db(models.Model):
+    ip=models.IPAddressField(blank=False)
+    mac=models.CharField(max_length=20,blank=False)
+    start=models.DateTimeField(blank=False)
+    end=models.DateTimeField(blank=False)
+    uid=models.CharField(max_length=100,blank=True)
+    client=models.CharField(max_length=100,blank=True)
+
+    class Meta:
+        db_table="lease_db"
