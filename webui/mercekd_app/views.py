432c8ceca3b373ad8c83b303e078783d19eebbb4
halilbay <halilbay91@gmail.com>
2012-07-10 11:08:19 +0300

    Fonksiyonlar oluşturuldu, url'ler tanımlandı ve html dosyaları oluşturuldu

--- webui/mercekd_app/views.py
+++ webui/mercekd_app/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+# Create your views here
+#
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+
+def home(request):
+    context = dict()
+    context['message'] = "Hello"
+    return render_to_response("home.html",
+        context_instance=RequestContext(request, context))
+
+def anomalies(request):
+    pass
+
+def leases(request):
+    pass
--- webui/webui/urls.py
+++ webui/webui/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,8 +7,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'webui.views.home', name='home'),
-    # url(r'^webui/', include('webui.foo.urls')),
+    url(r'^$', 'mercekd_app.views.home', name='home'),
+    url(r'^dns/', include('mercekd_app.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -15,3 +16,9 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^mercekd_media/(?P<path>.*)$', 'django.views.static.serve',
+             {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    )
