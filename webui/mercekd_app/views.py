53b8d8320fc8b269d71a95036c7625d8cfce77a2
halilbay <halilbay91@gmail.com>
2012-07-12 16:31:53 +0300

    leases fonksiyonun subnet'lere ayırma kısmı bitti.

--- webui/mercekd_app/views.py
+++ webui/mercekd_app/views.py
@@ -1,7 +1,6 @@
-# Create your views here
-#
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from mercekd_app.models import lease_db
 
 def home(request):
     context = dict()
@@ -12,5 +11,24 @@ def home(request):
 def anomalies(request):
     pass
 
+
 def leases(request):
-    pass
+    ip_list=[]
+    subnet_list=[]
+    lease_list=lease_db.objects.all()[:5]
+    for lease in lease_list:
+        ip_list.append(lease.ip)
+    for ip in ip_list:
+        octet_firstTwo=str(ip).split(".")[0]+"."+str(ip).split(".")[1]
+        octet_third=str(ip).split(".")[2]
+        subnet_name=str(octet_firstTwo+"."+octet_third+".x/24")
+        if subnet_name in subnet_list:
+            pass
+        else:
+            subnet_list.append(subnet_name)
+    for subnet_name in subnet_list:
+        subnet_name=[]
+
+
+
+
