1ea2c2127b80a17cda50491a56f21fd530115bf5
farukuzun <farukuzun@mail.com>
2012-11-30 16:07:28 +0200

    Use TextField instead of CharField in models.

--- mercekdUI/main/models.py
+++ mercekdUI/main/models.py
@@ -3,20 +3,20 @@ from django.db import models
 class Lease(models.Model):
      ip = models.ForeignKey('Lease_IP',null=True)
      mac = models.ForeignKey('Lease_Mac',null=True)
-     starts = models.DateTimeField(null=True)
+     starts = models.DateTimeField(null=True,max_length=0)
      ends = models.DateTimeField(null=True)
-     uid = models.CharField(null=True)
-     client = models.CharField(null=True)
+     uid = models.TextField(null=True)
+     client = models.TextField(null=True)
 
 class Lease_IP(models.Model):
-     v4 = models.CharField(null=True)
-     ip_name = models.CharField(null=True)
+     v4 = models.TextField(null=True)
+     ip_name = models.TextField(null=True)
 
 class Lease_Mac(models.Model):
-     mac = models.CharField(null=True)
-     mac_name = models.CharField(null=True)
+     mac = models.TextField(null=True)
+     mac_name = models.TextField(null=True)
 
 class LeasesFilePath(models.Model):
-     path = models.CharField(null=True)
+     path = models.TextField(null=True)
 
 # Create your models here.
